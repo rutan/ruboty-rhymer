eb2a316f537cad0250d80b641d1b47ed6e3c650b
ru_shalm <ru_shalm@hazimu.com>
2016-05-25 01:21:47 +0900

    check score

--- lib/ruboty/actions/rhymer/hearing.rb
+++ lib/ruboty/actions/rhymer/hearing.rb
@@ -5,17 +5,37 @@ module Ruboty
     module Rhymer
       class Hearing < ::Ruboty::Actions::Base
         def call(store)
-          store.push message.original[:body]
-          rhyme = try_rhymer(store.join('。'))
+          store.concat split_message(message.original[:body])
+          rhyme = try_rhymer(store)
           return unless rhyme
           store.clear
-          message.reply("#{rhyme[0]}　#{rhyme[1]}")
+          message.reply(generate_reply(rhyme))
         end
 
         private
 
-        def try_rhymer(message)
-          ::Rhymer::Parser.new(message).rhymes.first
+        def split_message(str)
+          str
+            .split(/[[:space:]。！？\.]+/)
+            .select { |n| n.size >= 4 }
+        end
+
+        def generate_reply(rhyme)
+          buf = []
+          buf.push 'Yo!'
+          buf.push rhyme[0]
+          buf.push rhyme[1]
+          buf.join('　')
+        end
+
+        def try_rhymer(messages)
+          ::Rhymer::Parser.new(messages.join("。\n")).rhymes.select do |rhyme|
+            rhyme.last >= score_border
+          end.sample
+        end
+
+        def score_border
+          (ENV['RHYMER_SCORE'] || 10).to_i
         end
       end
     end
--- lib/ruboty/handlers/rhymer.rb
+++ lib/ruboty/handlers/rhymer.rb
@@ -1,6 +1,8 @@
 module Ruboty
   module Handlers
     class Rhymer < Base
+      env :RHYMER_SCORE, 'Rhymer score border', optional: true
+
       on(/(?:.+)/,
          name: 'hearing',
          all: true,
